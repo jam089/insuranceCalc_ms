3525c909337cb0395a9e7ce3b84c9e330a136c05
jam089 <jam089@mail.ru>
2024-12-01 02:41:57 +0300

    create config.py with Settings and RunConfig. add to init file

---
+++ app/core/config.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+    reload: bool = True
+
+
+class Settings(BaseSettings):
+    run: RunConfig = RunConfig()
+
+
+settings = Settings()
