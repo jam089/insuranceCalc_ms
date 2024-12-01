11bde6e2358422a24ab72af407d350425bd08bb0
jam089 <jam089@mail.ru>
2024-12-01 03:46:40 +0300

    add DBConfig

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel
-from pydantic_settings import BaseSettings
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class RunConfig(BaseModel):
@@ -8,8 +8,24 @@ class RunConfig(BaseModel):
     reload: bool = True
 
 
+class DBConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 5
+    max_overflow: int = 2
+
+
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=[".env.template", ".env"],
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="insurance__",
+    )
+
     run: RunConfig = RunConfig()
+    db: DBConfig
 
 
 settings = Settings()
