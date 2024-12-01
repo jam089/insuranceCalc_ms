b4e07ee406774852575e2de7a8da72577c0e62fe
jam089 <jam089@mail.ru>
2024-12-01 23:13:17 +0300

    create administration.py route file with import_rates endpoint

---
+++ app/api/v1/routes/administration.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db import db_helper
+from crud.rate import bulk_load_rates
+from utils.files_utils import json_read
+from core import settings
+
+router = APIRouter(prefix="/administration", tags=["Administration"])
+
+
+@router.get("/import_rates/", status_code=status.HTTP_204_NO_CONTENT)
+async def import_rates(
+    db_sess: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    rates_dict = json_read(settings.import_.path)
+    if not await bulk_load_rates(db_sess, rates_dict):
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="import operation went wrong",
+        )
