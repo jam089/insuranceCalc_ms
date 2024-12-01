149a84c2f912514b2b9159df20c3a7e5f8d5fc22
jam089 <jam089@mail.ru>
2024-12-01 18:33:24 +0300

    correction expire_oncommit -> expire_on_commit for session_factory

--- app/db/db_helper.py
+++ app/db/db_helper.py
@@ -30,7 +30,7 @@ class DatabaseHelper:
         self.session_factory = async_sessionmaker(
             bind=self.engine,
             autoflush=False,
-            expire_oncommit=False,
+            expire_on_commit=False,
         )
 
     async def dispose(self) -> None:
