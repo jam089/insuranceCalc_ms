7de4b2c56ff85a02c5f23959ea7cce4df080a7ae
jam089 <jam089@mail.ru>
2024-12-01 18:34:29 +0300

    create rate.py crud file with get_insurance_rate_for_calc crud operation

---
+++ app/crud/rate.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from sqlalchemy import select, Result, and_
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.models import Rate
+
+
+async def get_insurance_rate_for_calc(
+    db_sess: AsyncSession,
+    calc_request_in: BaseModel,
+) -> Rate | None:
+    stmt = select(Rate).where(
+        and_(
+            Rate.data == calc_request_in.date,
+            Rate.cargo_type == calc_request_in.cargo_type,
+        )
+    )
+    result: Result = await db_sess.execute(stmt)
+    rate = result.scalar()
+    return rate
