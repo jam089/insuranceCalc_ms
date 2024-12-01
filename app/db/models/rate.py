4fe12ba5f73bdf71363516a325fcf0a1a3d89810
jam089 <jam089@mail.ru>
2024-12-01 23:06:52 +0300

    delete mapping_column() with unique param from date field. add UniqueConstraint for date+cargo_type field

--- app/db/models/rate.py
+++ app/db/models/rate.py
@@ -1,14 +1,17 @@
 from datetime import date
 
-from sqlalchemy import Date
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.orm import Mapped
 
 from .base import Base
 
 
 class Rate(Base):
     __tablename__ = "insurance_rates"
+    __table_args__ = (
+        UniqueConstraint("date", "cargo_type", name="uix_date_cargo_type"),
+    )
 
-    date: Mapped[date] = mapped_column(Date, unique=True)
+    date: Mapped[date]
     cargo_type: Mapped[str]
     rate: Mapped[float]
