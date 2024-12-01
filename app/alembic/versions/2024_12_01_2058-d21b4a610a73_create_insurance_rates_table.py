a60bfe9274ba53f03d13f506d756e4d0087b7d67
jam089 <jam089@mail.ru>
2024-12-01 20:59:32 +0300

    delete old migration for insurance_rates and gen new

--- app/alembic/versions/2024_12_01_2058-d21b4a610a73_create_insurance_rates_table.py
+++ app/alembic/versions/2024_12_01_2058-d21b4a610a73_create_insurance_rates_table.py
@@ -1,8 +1,8 @@
 """create insurance_rates table
 
-Revision ID: b9e8811c69fc
+Revision ID: d21b4a610a73
 Revises: 
-Create Date: 2024-12-01 04:57:58.157057
+Create Date: 2024-12-01 20:58:13.567604
 
 """
 
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = "b9e8811c69fc"
+revision: str = "d21b4a610a73"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -22,11 +22,12 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     op.create_table(
         "insurance_rates",
-        sa.Column("data", sa.Date(), nullable=False),
+        sa.Column("date", sa.Date(), nullable=False),
         sa.Column("cargo_type", sa.String(), nullable=False),
         sa.Column("rate", sa.Float(), nullable=False),
         sa.Column("id", sa.Integer(), nullable=False),
         sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("date"),
     )
 
 
