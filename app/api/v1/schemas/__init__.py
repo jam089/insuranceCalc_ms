448a870ea6b1fe127d33ec567706e53b36ffe046
jam089 <jam089@mail.ru>
2024-12-02 02:41:41 +0300

    add rate schemas to init file

--- app/api/v1/schemas/__init__.py
+++ app/api/v1/schemas/__init__.py
@@ -1,7 +1,12 @@
 __all__ = (
     "CalcRequest",
     "CalcResponse",
+    "CreateRate",
+    "UpdateRate",
+    "UpdateRatePartial",
+    "ViewRate",
 )
 
 
 from .insurance_calc import CalcRequest, CalcResponse
+from .rate import CreateRate, UpdateRate, UpdateRatePartial, ViewRate
