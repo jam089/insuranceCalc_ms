7e6cac6d536837da16be0c33b9848b5992e9ff71
jam089 <jam089@mail.ru>
2024-12-01 18:35:53 +0300

    add crud get_insurance_rate_for_calc and HTTPException fot insurance_calculation endpoint

--- app/api/v1/routes/insurance_calc.py
+++ app/api/v1/routes/insurance_calc.py
@@ -1,17 +1,36 @@
-from fastapi import APIRouter
+from typing import Annotated
 
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db import db_helper
 from api.v1.schemas import CalcResponse, CalcRequest
+from crud.rate import get_insurance_rate_for_calc
+from db.models import Rate
 
 router = APIRouter(prefix="/calc", tags=["Insurance Calculation"])
-
-MOCK_RATE = 0.5
 
 
 @router.post("/", response_model=CalcResponse)
 async def insurance_calculation(
+    db_sess: Annotated[AsyncSession, Depends(db_helper.session_getter)],
     calc_in: CalcRequest,
 ):
-    insurance_value = calc_in.declared_value * MOCK_RATE
-    return {
-        "insurance_value": insurance_value,
-    }
+    rate: Rate = await get_insurance_rate_for_calc(
+        db_sess,
+        calc_request_in=calc_in,
+    )
+
+    if rate:
+        insurance_value = calc_in.declared_value * rate.rate
+
+        return {
+            "request": calc_in,
+            "insurance_value": insurance_value,
+        }
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"insurance rate for {calc_in.cargo_type} "
+        f"on {calc_in.date} not found",
+    )
