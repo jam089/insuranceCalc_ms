c49acfecd9b4da805d226bdb4147751056b382de
jam089 <jam089@mail.ru>
2024-12-02 04:04:23 +0300

    include rate_router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from .routes.insurance_calc import router as insurance_router
 from .routes.administration import router as admin_router
+from .routes.rate import router as rate_router
 
 router = APIRouter(prefix="/v1")
 
 router.include_router(insurance_router)
 router.include_router(admin_router)
+router.include_router(rate_router)
