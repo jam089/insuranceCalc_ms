83bb906d0a257770e8bb73e8d7aceb130913cb9d
jam089 <jam089@mail.ru>
2024-12-03 02:56:09 +0300

    add task batch_sender to start up event

--- app/main.py
+++ app/main.py
@@ -1,11 +1,30 @@
+import asyncio
+import logging
+from contextlib import asynccontextmanager
+
 import uvicorn
+from aiokafka.errors import KafkaConnectionError
 from fastapi import FastAPI
 
 from core import settings
 from api import router as api_router
+from services import kafka
+
+logger = logging.getLogger("uvicorn")
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        await kafka.producer.start()
+        asyncio.create_task(kafka.producer.batch_sender())
+    except KafkaConnectionError as ex:
+        logger.critical(ex.args)
+    yield
+    await kafka.producer.stop()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(api_router)
 
