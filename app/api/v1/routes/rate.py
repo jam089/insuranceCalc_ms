dc8d12935f942af4dee46cbeec2c2cfe91d22d97
jam089 <jam089@mail.ru>
2024-12-04 03:25:38 +0300

    change path root path of router from rate to rates

--- app/api/v1/routes/rate.py
+++ app/api/v1/routes/rate.py
@@ -14,19 +14,7 @@ from crud import rate as rate_crud
 
 logger = logging.getLogger("uvicorn")
 
-router = APIRouter(prefix="/rate", tags=["Insurance Rates"])
-
-
-@router.get("/date/{date}/", response_model=Sequence[ViewRate])
-async def get_rate_by_date(
-    db_sess: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-    date: str,
-):
-    logger.critical(date)
-    return await rate_crud.get_insurance_rate_by_date(
-        db_sess,
-        date=datetime.strptime(date, "%Y-%m-%d").date(),
-    )
+router = APIRouter(prefix="/rates", tags=["Insurance Rates"])
 
 
 @router.get("/{rate_id}/", response_model=ViewRate)
