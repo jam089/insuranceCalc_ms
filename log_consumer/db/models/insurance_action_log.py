408629e853fe03470e553db25cb2795ccc04f2e8
jam089 <jam089@mail.ru>
2024-12-05 02:43:17 +0300

    create insurance_action_log.py with InsuranceActionLog model

---
+++ log_consumer/db/models/insurance_action_log.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime
+from sqlalchemy.orm import mapped_column, Mapped
+
+from .base import Base
+
+
+class InsuranceActionLog(Base):
+    __tablename__ = "insurance_action_log"
+
+    user_id: Mapped[int | None] = mapped_column(default=None)
+    action: Mapped[str]
+    date_time: Mapped[datetime] = mapped_column(DateTime)
+
+    __mapper_args__ = {
+        "order_by": date_time.desc(),
+    }
