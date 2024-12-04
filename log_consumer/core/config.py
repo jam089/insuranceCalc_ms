d14c09b656641c245207d6976cdc4af552abb6c8
jam089 <jam089@mail.ru>
2024-12-04 23:23:18 +0300

    create config.py with DB and Run config, add to init

---
+++ log_consumer/core/config.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8050
+    reload: bool = True
+
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 5
+    max_overflow: int = 2
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=[".env.template", ".env"],
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="consumer__",
+    )
+
+    run: RunConfig = RunConfig()
+    db: DatabaseConfig
+
+
+settings = Settings()
