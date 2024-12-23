d00d77146ba54021cf4ec7461a185a9669d4b812
jam089 <jam089@mail.ru>
2024-12-24 02:16:16 +0300

    add datatype for alchemy model

--- consumer_service/log_consumer/db/models/insurance_action_log.py
+++ consumer_service/log_consumer/db/models/insurance_action_log.py
@@ -1,17 +1,41 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime
+from sqlalchemy import DateTime, TypeDecorator, Integer
 from sqlalchemy.orm import mapped_column, Mapped
 
 from .base import Base
 
 
+class DateTimeString(TypeDecorator):
+    impl = DateTime
+
+    def process_bind_param(self, value, dialect):
+        if isinstance(value, str):
+            try:
+                return datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+            except ValueError:
+                return None
+        return value
+
+
+class IntegerString(TypeDecorator):
+    impl = Integer
+
+    def process_bind_param(self, value, dialect):
+        if isinstance(value, str):
+            try:
+                return int(value)
+            except ValueError:
+                return None
+        return value
+
+
 class InsuranceActionLog(Base):
     __tablename__ = "insurance_action_logs"
 
-    user_id: Mapped[int | None] = mapped_column(default=None)
+    user_id: Mapped[int | None] = mapped_column(IntegerString, default=None)
     action: Mapped[str]
-    date_time: Mapped[datetime] = mapped_column(DateTime)
+    date_time: Mapped[datetime] = mapped_column(DateTimeString)
 
     @classmethod
     def __default_order_by__(cls):
