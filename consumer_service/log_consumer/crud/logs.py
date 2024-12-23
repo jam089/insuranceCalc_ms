55e4d41d5db14027b19349704f2a47c8c53c0f90
jam089 <jam089@mail.ru>
2024-12-24 02:50:28 +0300

    add get_logs, bulk_load_logs crud

--- consumer_service/log_consumer/crud/logs.py
+++ consumer_service/log_consumer/crud/logs.py
@@ -1,10 +1,15 @@
+import logging
 from datetime import datetime
 from typing import Sequence
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, ScalarResult
+from sqlalchemy import select, ScalarResult, insert
+from sqlalchemy.exc import SQLAlchemyError
 
 from log_consumer.db.models import InsuranceActionLog
+
+
+logger = logging.getLogger("uvicorn")
 
 
 async def get_logs(
@@ -24,3 +29,29 @@ async def get_logs(
     stmt = select(InsuranceActionLog).where(*conditions)
     result: ScalarResult = await db_sess.scalars(stmt)
     return result.all()
+
+
+async def get_last_logs(
+    db_sess: AsyncSession,
+    limit: int,
+) -> Sequence[InsuranceActionLog]:
+    stmt = select(InsuranceActionLog).limit(limit=limit)
+    result: ScalarResult = await db_sess.scalars(stmt)
+    return result.all()
+
+
+async def bulk_load_logs(
+    db_sess: AsyncSession,
+    logs: list,
+) -> bool:
+    try:
+        stmt = insert(InsuranceActionLog).values(logs)
+        await db_sess.execute(stmt)
+        await db_sess.commit()
+
+        return True
+
+    except SQLAlchemyError as ex:
+        logger.error(ex.args)
+        await db_sess.rollback()
+        return False
