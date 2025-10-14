c5418e97f2f2ac1dbeba56a85134d9ed408d3477
Ivan Filippov <jam089@mail.ru>
2025-10-14 17:23:04 +0300

    first unit. for json_read

---
+++ tests/unit/test_utils_files_utils.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+import json
+
+import pytest
+
+from utils.files_utils import json_read
+
+
+test_json = {
+    "parents": [
+        {
+            "name": "Homer",
+            "surname": "Simpson",
+            "age": "34",
+        },
+        {
+            "name": "Marge",
+            "surname": "Simpson",
+            "age": "39",
+        },
+    ],
+    "kids": [
+        {
+            "name": "Lisa",
+            "surname": "Simpson",
+            "age": "8",
+        },
+        {
+            "name": "Bart",
+            "surname": "Simpson",
+            "age": "10",
+        },
+    ],
+}
+
+
+@pytest.mark.asyncio
+async def test_json_read(tmp_path: Path) -> None:
+    file_path = tmp_path / "data.json"
+    file_path.write_text(json.dumps(test_json), encoding="utf-8")
+
+    response = json_read(file_path)
+
+    assert isinstance(response, dict)
+    assert test_json == response
