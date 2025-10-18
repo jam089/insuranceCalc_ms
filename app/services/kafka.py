1f1cadd03621a2299b8e8cccce5e25962984e28d
Ivan Filippov <jam089@mail.ru>
2025-10-18 17:21:29 +0300

    remove useless envs

--- app/services/kafka.py
+++ app/services/kafka.py
@@ -6,11 +6,11 @@ from random import choice
 
 from aiokafka import AIOKafkaProducer
 from aiokafka.admin import AIOKafkaAdminClient, NewTopic
-from aiokafka.errors import KafkaConnectionError
+from aiokafka.errors import KafkaConnectionError, TopicAlreadyExistsError
 from aiokafka.producer.message_accumulator import BatchBuilder
 from core import settings
 
-logger = logging.getLogger("uvicorn")
+logger = logging.getLogger("uvicorn.kafka")
 
 
 class KafkaProducer:
@@ -18,65 +18,79 @@ class KafkaProducer:
         self,
         bootstrap_servers: str,
         topic: str,
-        max_batch_size: int = 16384,
-        linger_ms: int = 0,
     ):
         self.bootstrap_servers = bootstrap_servers
         self.producer: AIOKafkaProducer | None = None
         self.admin: AIOKafkaAdminClient | None = None
         self.topic = topic
         self.batch: BatchBuilder | None = None
-        self.massage_queue: asyncio.Queue = asyncio.Queue()
-        self.max_batch_size = max_batch_size
-        self.linger_ms = linger_ms
+        self.message_queue: asyncio.Queue = asyncio.Queue()
+        self.max_batch_size = settings.kafka_logger.max_batch_size
+        self.linger_ms = settings.kafka_logger.linger_ms
+        self._running = False
+        self.enabled = settings.kafka_logger.enable
 
     async def start(self) -> None:
-        if settings.kafka_logger.enable:
-            self.producer = AIOKafkaProducer(
-                bootstrap_servers=self.bootstrap_servers,
-                max_batch_size=self.max_batch_size,
-                linger_ms=self.linger_ms,
-            )
-            self.admin = AIOKafkaAdminClient(bootstrap_servers=self.bootstrap_servers)
-            for counter in range(0, 10):
-                try:
-                    await self.producer.start()
-                    asyncio.create_task(self.batch_sender())
-                    break
-                except KafkaConnectionError as ex:
-                    await self.stop()
-                    logger.error(f"{counter}: {ex.args}")
-                    await asyncio.sleep(delay=3)
-            metadata = await self.producer.client.fetch_all_metadata()
-            if self.topic not in metadata.topics():
-                await self._new_topic()
-            self.batch = self.producer.create_batch()
+        if not self.enabled:
+            return
+        self.producer = AIOKafkaProducer(
+            bootstrap_servers=self.bootstrap_servers,
+            max_batch_size=self.max_batch_size,
+            linger_ms=self.linger_ms,
+        )
+        self.admin = AIOKafkaAdminClient(bootstrap_servers=self.bootstrap_servers)
+        for counter in range(10):
+            try:
+                await self.producer.start()
+                self._running = True
+                asyncio.create_task(self._batch_sender(), name="kafka_log_worker")
+                break
+            except KafkaConnectionError as ex:
+                logger.error(f"[Kafka] connection attempt {counter} failed: {ex}")
+                await asyncio.sleep(3)
+
+        await self._ensure_topic()
+        logger.info("[Kafka] started")
 
     async def stop(self) -> None:
-        if settings.kafka_logger.enable and self.producer:
+        if not self.enabled:
+            return
+        self._running = False
+        if self.producer:
+            await self.producer.flush()
             await self.producer.stop()
+            logger.info("[Kafka] stoped")
 
-    async def batch_sender(self) -> None:
-        while True:
+    async def _batch_sender(self) -> None:
+        if not self.producer:
+            return
+        while self._running:
             try:
-                msg = await asyncio.wait_for(self.massage_queue.get(), timeout=15)
+                msg = await asyncio.wait_for(self.message_queue.get(), timeout=10)
+                await self.producer.send(self.topic, msg)
+                logger.debug(f"[Kafka] message sent: {msg}")
+            except asyncio.TimeoutError:
+                logger.debug("[Kafka] queue is empty (timeout)")
 
-                metadata = (
-                    self.batch.append(key=None, value=msg, timestamp=None)
-                    if self.batch
-                    else None
-                )
-                if metadata is None and self.producer:
-                    partitions = await self.producer.partitions_for(self.topic)
-                    chosen_partition = choice(tuple(partitions))
-                    await self.producer.send_batch(
-                        self.batch,
-                        self.topic,
-                        partition=chosen_partition,
-                    )
-                    self.batch = self.producer.create_batch()
-            except TimeoutError:
-                logger.debug("message queue for kafka logging is empty")
+    async def _send_batch(self) -> None:
+        if self.producer:
+            partitions = await self.producer.partitions_for(self.topic)
+            chosen_partition = choice(tuple(partitions))
+            await self.producer.send_batch(
+                self.batch,
+                self.topic,
+                partition=chosen_partition,
+            )
+            logger.info("[Kafka] batch sent")
+            self.batch = self.producer.create_batch()
+
+    async def _ensure_topic(self) -> None:
+        if not self.enabled or not self.producer:
+            return
+        await self.producer.client.bootstrap()
+        metadata = await self.producer.client.fetch_all_metadata()
+        if self.topic not in metadata.topics():
+            await self._new_topic()
 
     async def k_logger(
         self,
@@ -84,14 +98,18 @@ class KafkaProducer:
         date_time: datetime,
         user_id: int | None = None,
     ) -> None:
-        if settings.kafka_logger.enable:
-            msg = {
-                "action": crud_action,
-                "date_time": date_time.strftime("%Y-%m-%d"),
-            }
-            if user_id:
-                msg.update(user_id=str(user_id))
-            await self.massage_queue.put(json.dumps(msg).encode(encoding="utf-8"))
+        logger.debug("[Kafka] action was logged")
+        if not self.enabled:
+            logger.debug("[Kafka] enable setting is false")
+            return
+        msg = {
+            "action": crud_action,
+            "date_time": date_time.strftime("%Y-%m-%d"),
+        }
+        if user_id:
+            msg["user_id"] = str(user_id)
+        await self.message_queue.put(json.dumps(msg).encode(encoding="utf-8"))
+        logger.debug("[Kafka] message in queue")
 
     async def _new_topic(self) -> None:
         if self.admin:
@@ -106,7 +124,9 @@ class KafkaProducer:
 
                 await self.admin.create_topics([new_topic])
             except KeyError as ex:
-                logger.error(ex.args)
+                logger.error(f"[Kafka] {ex.args}")
+            except TopicAlreadyExistsError:
+                logger.error(f"[Kafka] Topic {self.topic} already exists")
             finally:
                 await self.admin.close()
 
@@ -114,6 +134,4 @@ class KafkaProducer:
 producer = KafkaProducer(
     bootstrap_servers=str(settings.kafka_logger.bootstrap_servers),
     topic=settings.kafka_logger.topic,
-    max_batch_size=settings.kafka_logger.max_batch_size,
-    linger_ms=settings.kafka_logger.linger_ms,
 )
